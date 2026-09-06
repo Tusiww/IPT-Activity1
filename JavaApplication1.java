e83ac8679f099bc049d952dac831eb8abceefbaf
Tusiww <rportales121@gmail.com>
2026-09-06 14:55:54 +0800

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,55 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+
+       public static void main(String[] args) {
+        String prod1Name = "Fresh Milk 1L";
+        String prod1Brand = "Alaska";
+        String prod1Manu = "Alaska";
+        String prod1Exp = "2026-09-20";
+        double prod1Price = 89.5;
+        int prod1Quantity = 40;
+        
+        System.out.println("==================================");
+        System.out.println("      PRODUCT DETAILS OUTPUT      ");
+        System.out.println("==================================");
+        System.out.println("\n=== Product 1 ===");
+        System.out.println("Name: " + prod1Name);
+        System.out.println("Brand: " + prod1Brand);
+        System.out.println("Manufacturer: " + prod1Manu);
+        System.out.println("Price: " + prod1Price);
+        System.out.println("Quantity: " + prod1Quantity);
+        System.out.println("Expiration Date: " + prod1Exp);
+        
+        String prod2Name = "Instant Noodles";
+        String prod2Brand = "Lucky Me!";
+        String prod2Manu = "Monde Nissin Corporation";
+        String prod2Exp = "2027-03-15";
+        double prod2Price = 15.0;
+        int prod2Quantity = 150;
+        
+        System.out.println("\n=== Product 2 ===");
+        System.out.println("Name: " + prod2Name);
+        System.out.println("Brand: " + prod2Brand);
+        System.out.println("Manufacturer: " + prod2Manu);
+        System.out.println("Price: " + prod2Price);
+        System.out.println("Quantity: " + prod2Quantity);
+        System.out.println("Expiration Date: " + prod2Exp);
+        
+        String prod3Name = "Wireless Mouse";
+        String prod3Brand = "Logitech";
+        String prod3Manu = "Logitech International S.A.";
+        String prod3Exp = "N/A";
+        double prod3Price = 599.75;
+        int prod3Quantity = 25;
+        
+        System.out.println("\n=== Product 3 ===");
+        System.out.println("Name: " + prod3Name);
+        System.out.println("Brand: " + prod3Brand);
+        System.out.println("Manufacturer: " + prod3Manu);
+        System.out.println("Price: " + prod3Price);
+        System.out.println("Quantity: " + prod3Quantity);
+        System.out.println("Expiration Date: " + prod3Exp);
+    }
+    
+}
